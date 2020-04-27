f6ed786c42f4658e1fc53ae53fc9582360cc63c8
imvenj <im.venj@gmail.com>
2020-04-27 17:01:32 +0800

    WIP.

--- my_work_timer/lib/main.dart
+++ my_work_timer/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:my_work_timer/widgets.dart';
 
 void main() => runApp(MyApp());
+
+void emptyMethod() {}
 
 class MyApp extends StatelessWidget {
   @override
@@ -10,13 +13,63 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My Work Timer'),
-        ),
-        body: Center(
-          child: Text('My Work Timer')
-        )
+      home: TimerHomePage()
+    );
+  }
+}
+
+class TimerHomePage extends StatelessWidget {
+  final double defaultPadding = 5.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Work Timer'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(defaultPadding)
+              ),
+              Expanded(
+                child: ProductivityButton(
+                  color: Color(0xff009688),
+                  text: 'Work',
+                  onPressed: emptyMethod,
+                  size: 150
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(defaultPadding)
+              ),
+              Expanded(
+                child: ProductivityButton(
+                  color: Color(0xff607d8b),
+                  text: 'Short Break',
+                  onPressed: emptyMethod,
+                  size: 150
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(defaultPadding)
+              ),
+              Expanded(
+                child: ProductivityButton(
+                  color: Color(0xff455a64),
+                  text: 'Long Break',
+                  onPressed: emptyMethod,
+                  size: 150
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(defaultPadding)
+              ),
+            ]
+          )
+        ]
       )
     );
   }
