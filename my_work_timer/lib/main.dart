592b15b433b371d38fde87296b69794ad19bcee9
imvenj <im.venj@gmail.com>
2020-04-27 22:25:06 +0800

    Count down works.

--- my_work_timer/lib/main.dart
+++ my_work_timer/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:my_work_timer/timer.dart';
+import 'package:my_work_timer/timermodel.dart';
 import 'package:my_work_timer/widgets.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 
@@ -21,9 +23,11 @@ class MyApp extends StatelessWidget {
 
 class TimerHomePage extends StatelessWidget {
   final double defaultPadding = 5.0;
+  final CountDownTimer timer = CountDownTimer();
 
   @override
   Widget build(BuildContext context) {
+    timer.startWork();
     return Scaffold(
       appBar: AppBar(
         title: Text('My Work Timer'),
@@ -74,15 +78,22 @@ class TimerHomePage extends StatelessWidget {
                 ]
               ),
               Expanded(
-                child: CircularPercentIndicator(
-                  radius: availableWidth / 2,
-                  lineWidth: 10.0,
-                  percent: 1,
-                  center: Text(
-                    '30:00',
-                    style: Theme.of(context).textTheme.display1
-                  ),
-                  progressColor: Color(0xff009688)
+                child: StreamBuilder(
+                  initialData: '00:00',
+                  stream: timer.stream(),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    TimerModel timer = snapshot.data == '00:00' ? TimerModel('00:00', 1) : snapshot.data;
+                    return CircularPercentIndicator(
+                      radius: availableWidth / 2,
+                      lineWidth: 10.0,
+                      percent: timer.percent,
+                      center: Text(
+                        timer.time,
+                        style: Theme.of(context).textTheme.display1
+                      ),
+                      progressColor: Color(0xff009688)
+                    );
+                  },
                 )
               ),
               Row(
